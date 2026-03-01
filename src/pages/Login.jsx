aeb4df2d7cbdfb96fadfaeec673bed3adb1cd625
Ayushi-Pattanaik-28 <ayushipattanaik94458@gmail.com>
2026-03-02 04:56:17 +0530

    final commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,6 +20,9 @@ export default function Login() {
       }
 
       login(res.data);
+
+      toast("Login successful");
+
       navigate("/dashboard");
     } catch (err) {
       toast("Login failed");
