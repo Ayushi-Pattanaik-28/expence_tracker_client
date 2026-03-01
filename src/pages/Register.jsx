b1633f8b25ff154317e93fb89b9d2d196a3a7ef8
Ayushi-Pattanaik-28 <ayushipattanaik94458@gmail.com>
2026-03-02 04:12:12 +0530

    add register file

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,7 +20,7 @@ export default function Register() {
       toast("Account created successfully");
       navigate("/");
     } catch (err) {
-      toast("Signup failed"); 
+      toast("Signup failed"); a
     }
   };
 
